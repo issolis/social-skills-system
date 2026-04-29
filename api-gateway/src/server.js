86121eefee8a7d585d39c7cacfb5a5174a090ba4
fabrimena <fabriciomenamejia@gmail.com>
2026-04-29 01:46:20 -0600

    FIX: Se agrega el URL del servicio de autenticación en el API Gateway.

--- api-gateway/src/server.js
+++ api-gateway/src/server.js
@@ -5,8 +5,9 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`[API GATEWAY] Running on port ${PORT}`);
+    console.log(`[API GATEWAY] Auth service: ${process.env.AUTH_SERVICE_URL}`);
     console.log(`[API GATEWAY] Users service: ${process.env.USERS_SERVICE_URL}`);
     console.log(`[API GATEWAY] Skills service: ${process.env.SKILLS_SERVICE_URL}`);
     console.log(`[API GATEWAY] Orders service: ${process.env.ORDERS_SERVICE_URL}`);
